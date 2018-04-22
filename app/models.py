c4ef04601d25fa5df3ef31cfa0469e21ea4d322d
mark <murimimg180@gmail.com>
2018-04-22 16:47:07 +0300

    made insertions

--- app/models.py
+++ app/models.py
@@ -26,11 +26,22 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(255),unique = True, index =True)
     password_hash = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
-    posts = db.relationship("Post", backref="user", lazy = "dynamic")
     comment = db.relationship("Comments", backref="user", lazy = "dynamic")
 
+    @property
+    def password(self):
+        raise AttributeError('You can not read the password Attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    def _repr_(self):
+        return f'User {self.username}'
+        
 class Post(db.Model):
     """ List of pitches in each category """
 
