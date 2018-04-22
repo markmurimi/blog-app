bfccbb1940348295d11c427ac6e907608ff3ac3b
mark <murimimg180@gmail.com>
2018-04-22 16:21:25 +0300

    made insertions

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from . import login_manager
+from sqlalchemy.sql import func
 
 
 
@@ -17,5 +18,29 @@ class User(UserMixin,db.Model):
     pass_secure = db.Column(db.String(255))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    pitches = db.relationship("Pitch", backref="user", lazy = "dynamic")
+    posts = db.relationship("Post", backref="user", lazy = "dynamic")
     comment = db.relationship("Comments", backref="user", lazy = "dynamic")
+
+class Post(db.Model):
+    """ List of pitches in each category """
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,primary_key = True)
+    content = db.Column(db.String)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comment = db.relationship("Comments", backref="pitches", lazy = "dynamic")
+
+    def save_post(self):
+        ''' Save the posts '''
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def clear_pitches(cls):
+        Post.all_posts.clear()
+
+    def get_posts(id):
+        pitches = Pitch.query.filter_by(category_id=id).all()
+        return pitches
